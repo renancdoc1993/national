77764587485b5be089ee6c5ea090596b369e5c8c
renancdoc1993 <117864751+renancdoc1993@users.noreply.github.com>
2025-04-04 11:57:12 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,1 @@
-export const environment = {
-  firebase: {
-    production:false,
-    apiKey: "AIzaSyDvqIQMTvnYm6Rjdj4UtXJQnW6npVxf6Bw",
-    authDomain: "national-5d148.firebaseapp.com",
-    projectId: "national-5d148",
-    storageBucket: "national-5d148.firebasestorage.app",
-    messagingSenderId: "880546436060",
-    appId: "1:880546436060:web:ea559e9e067c4077a8b519",
-    measurementId: "G-Z52H1T1314"
-  }
-};
+
